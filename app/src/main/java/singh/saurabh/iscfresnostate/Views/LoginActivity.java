8db20095dcbe17f13445174eca1a97400ee1cdf1
Saurabh Singh <saurabh30590@gmail.com>
2017-03-08 22:48:31 -0800

    1. Firebase sdk added. 2. Facebook signin hooked up with Firebase authentication

--- app/src/main/java/singh/saurabh/iscfresnostate/Views/LoginActivity.java
+++ app/src/main/java/singh/saurabh/iscfresnostate/Views/LoginActivity.java
@@ -2,39 +2,80 @@ package singh.saurabh.iscfresnostate.Views;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-import singh.saurabh.iscfresnostate.Helpers.Util;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 import singh.saurabh.iscfresnostate.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Activity activity;
-    LoginButton loginButton;
-    CallbackManager callbackManager;
+    private String TAG;
+
+    // Firebase
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mFirebaseAuthListener;
+
+    // Facebook
+    private LoginButton mLoginButton;
+    private CallbackManager mCallbackManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        activity = this;
+        TAG = this.getLocalClassName();
 
-        callbackManager = CallbackManager.Factory.create();
+        // Initialize Firebase auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in: " + user.getUid());
+                    Toast.makeText(LoginActivity.this, "Welcome: " + user.getDisplayName()
+                            , Toast.LENGTH_SHORT).show();
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
 
-        loginButton = (LoginButton) findViewById(R.id.facebook_login_button);
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        // Initialize Facebook callback manager
+        mCallbackManager = CallbackManager.Factory.create();
+
+        mLoginButton = (LoginButton) findViewById(R.id.facebook_login_button);
+        mLoginButton.setReadPermissions("email", "public_profile");
+        mLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                finish();
-                startActivity(new Intent(activity, MainActivity.class));
+                handleFacebookAccessToken(AccessToken.getCurrentAccessToken());
             }
 
             @Override
@@ -49,9 +90,49 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken: " + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mFirebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete: " + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.e(TAG, "signInWithCredential: ex: " + task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed."
+                                    , Toast.LENGTH_SHORT).show();
+                        } else {
+
+                            finish();
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(mFirebaseAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mFirebaseAuthListener != null) {
+            mFirebaseAuth.removeAuthStateListener(mFirebaseAuthListener);
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/singh/saurabh/iscfresnostate/Views/MainActivity.java
+++ app/src/main/java/singh/saurabh/iscfresnostate/Views/MainActivity.java
@@ -3,6 +3,9 @@ package singh.saurabh.iscfresnostate.Views;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+
 import singh.saurabh.iscfresnostate.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
