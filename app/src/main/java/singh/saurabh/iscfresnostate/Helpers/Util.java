371c65370f514ef8c7fb40fdd89f71966c8dd605
Saurabh Singh <saurabh30590@gmail.com>
2017-03-11 15:55:29 -0800

    fixed up gitignore’s fuck up

--- app/src/main/java/singh/saurabh/iscfresnostate/Helpers/Util.java
+++ app/src/main/java/singh/saurabh/iscfresnostate/Helpers/Util.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 import com.facebook.AccessToken;
+import com.google.firebase.auth.FirebaseAuth;
 
 import singh.saurabh.iscfresnostate.R;
 
@@ -19,14 +20,15 @@ public class Util {
     }
 
     public static boolean isUserLoggedIn(Activity activity) {
-        SharedPreferences preferences = activity.getPreferences(Context.MODE_PRIVATE);
-        return preferences.contains(getKey(activity, R.string.login_key)) || AccessToken.getCurrentAccessToken() != null;
+//        SharedPreferences preferences = activity.getPreferences(Context.MODE_PRIVATE);
+//        return preferences.contains(getKey(activity, R.string.login_key)) || AccessToken.getCurrentAccessToken() != null;
+        return FirebaseAuth.getInstance().getCurrentUser() != null;
     }
 
     public static void setLoginPrefrence(Activity activity) {
-        SharedPreferences preferences = activity.getPreferences(Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putBoolean(getKey(activity, R.string.login_key), true);
-        editor.apply();
+//        SharedPreferences preferences = activity.getPreferences(Context.MODE_PRIVATE);
+//        SharedPreferences.Editor editor = preferences.edit();
+//        editor.putBoolean(getKey(activity, R.string.login_key), true);
+//        editor.apply();
     }
 }
